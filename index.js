7301f92bac29ff5858f04a8663ddadab24aed958
Daniel Mueller <danielpetermueller@gmail.com>
2021-05-26 09:18:23 -0400

    commit tree 164ba54e0f60a9d45c10f02cae651cdd90b3dd18

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(name, email){
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json" ,
+            "Accept": "application/json",
+},
+    body: JSON.stringify({
+     name,
+     email
+  }
+  )
+})
+.then(response => response.json())
+.then(function (newID){body.document.innerHTML = newID(['id'])})
+.catch((error) => {
+  console.error('Error:', error);
+});
+}
+
